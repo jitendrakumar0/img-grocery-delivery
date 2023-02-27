f8571d7d6ce5488baafd6331f4d5f969516a7d74
sushilimg <sushilchoudhary.img@gmail.com>
2023-02-27 16:44:02 +0530

    work on blog-single

--- assets/js/vendors/password.js
+++ assets/js/vendors/password.js
@@ -1,1 +1,34 @@
-var password=document.getElementById("fakePassword"),toggler=document.getElementById("passwordToggler");showHidePassword=()=>{"password"==password.type?(password.setAttribute("type","text"),toggler.classList.add("bi-eye"),toggler.classList.remove("bi-eye-slash")):(toggler.classList.remove("bi-eye"),toggler.classList.add("bi-eye-slash"),password.setAttribute("type","password"))},toggler.addEventListener("click",showHidePassword);
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+var password = document.getElementById("fakePassword"),
+  toggler = document.getElementById("passwordToggler");
+(showHidePassword = () => {
+  "password" == password.type
+    ? (password.setAttribute("type", "text"),
+      toggler.classList.add("bi-eye"),
+      toggler.classList.remove("bi-eye-slash"))
+    : (toggler.classList.remove("bi-eye"),
+      toggler.classList.add("bi-eye-slash"),
+      password.setAttribute("type", "password"));
+}),
+  toggler.addEventListener("click", showHidePassword);
+
--- assets/js/vendors/slick-slider.js
+++ assets/js/vendors/slick-slider.js
@@ -40,7 +40,7 @@ $(".category-slider").length &&
       infinite: !0,
       slidesToShow: 5,
       slidesToScroll: 1,
-      autoplay: !0,
+      autoplay: !1,
       dots: !1,
       arrows: !0,
       prevArrow:
@@ -48,9 +48,9 @@ $(".category-slider").length &&
       nextArrow:
         '<span class="slick-next "><i class="feather-icon icon-chevron-right "></i></span>',
       responsive: [
-        { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3 } },
+        { breakpoint: 1024, settings: { slidesToShow: 4, slidesToScroll: 4 } },
         { breakpoint: 820, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-        { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+        { breakpoint: 576, settings: { slidesToShow: 1, slidesToScroll: 1 } },
       ],
     }),
   $(".hero-slider").length &&
